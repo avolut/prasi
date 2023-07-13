fda50b742b5c61acad72243f184018977086df8f
Rizky <rizky05@gmail.com>
2023-07-13 10:02:31 +0700

    tree errorBoundary

--- app/web/src/compo/editor/content/tree/tree.tsx
+++ app/web/src/compo/editor/content/tree/tree.tsx
@@ -13,10 +13,11 @@ import { NodeContent, flattenTree } from "../../../page/tools/flatten-tree";
 import { MContent } from "../../../types/general";
 import { MRoot } from "../../../types/root";
 import { CETreeItem, DEPTH_WIDTH } from "./tree-item";
+import { ErrorBoundary } from "web-init/src/web/error-boundary";
 
 export const CETree: FC<{ id: string }> = ({ id }) => {
   const c = useGlobal(CEGlobal, id);
-  const local = useLocal({ childs: [] as string[] });
+  const local = useLocal({ childs: [] as string[], treeError: false });
   c.editor.tree.render = local.render;
   c.editor.tree.list = flattenTree(c.root as any);
 
@@ -68,138 +69,156 @@ export const CETree: FC<{ id: string }> = ({ id }) => {
           `
         )}
       >
-        <DndProvider backend={MultiBackend} options={getBackendOptions()}>
-          <div className="flex flex-col items-stretch bg-white right-0 left-0">
-            <TypedTree
-              ref={(el) => {
-                c.editor.tree.method = el;
-              }}
-              tree={c.editor.tree.list}
-              rootId={"root"}
-              insertDroppableFirst={false}
-              classes={{
-                container: "flex flex-col",
-                dropTarget: "drop-target",
-                placeholder: "placeholder",
-              }}
-              dragPreviewRender={DragPreview}
-              dropTargetOffset={10}
-              placeholderRender={(node, params) => (
-                <Placeholder node={node} params={params} />
-              )}
-              sort={false}
-              onDragStart={(node) => {
-                if (node.data?.content) {
-                  local.childs = walk(node.data?.content);
-                }
-              }}
-              canDrop={(_, { dragSource, dropTargetId, dropTarget }) => {
-                const parentSource = dragSource?.data?.content.parent
-                  .parent as MContent;
-                if (parentSource.get("id") === "root") {
-                  return false;
-                }
-                if (dropTargetId === "root") {
-                  if (
-                    dragSource?.data &&
-                    dragSource.data.content.get("type") === "section"
-                  ) {
-                    return true;
-                  }
-                  return false;
-                } else if (dragSource?.data && dropTarget?.data) {
-                  const from = dragSource.data.content.get("type");
-                  const to = dropTarget.data.content.get("type");
-
-                  if (
-                    local.childs.includes(
-                      dropTarget.data.content.get("id") || ""
-                    )
-                  ) {
-                    return false;
-                  }
-
-                  if (from === "section" || to === "text") {
-                    return false;
-                  } else if (from === "item") {
-                    if (to === "section" || to === "item") {
-                      return true;
-                    } else {
-                      return false;
-                    }
-                  } else if (from === "text") {
-                    if (to === "item") {
-                      return true;
-                    }
-                  }
-
-                  return false;
-                }
-              }}
-              onDrop={(
-                _,
-                { dropTarget, dropTargetId, dragSource, relativeIndex }
-              ) => {
-                if (
-                  dragSource?.data &&
-                  (dropTarget?.data || dropTargetId === "root")
-                ) {
-                  const from = dragSource.data;
-                  const insert = from.content.clone();
-                  from.content.parent.forEach((e, idx) => {
-                    if (e === from.content) {
-                      from.content.parent.delete(idx);
-                    }
-                  });
-
-                  if (dropTargetId !== "root") {
-                    const to = dropTarget?.data;
-                    if (to) {
-                      const childs = to.content.get("childs");
-                      if (childs && childs.length - 1 >= (relativeIndex || 0)) {
-                        childs?.insert(relativeIndex || 0, [insert]);
-                      } else {
-                        childs?.push([insert]);
+        <ErrorBoundary
+          onError={() => {
+            local.treeError = true;
+            local.render();
+            setTimeout(() => {
+              local.treeError = false;
+              local.render();
+            }, 300);
+          }}
+        >
+          {!local.treeError && (
+            <>
+              <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+                <div className="flex flex-col items-stretch bg-white right-0 left-0">
+                  <TypedTree
+                    ref={(el) => {
+                      c.editor.tree.method = el;
+                    }}
+                    tree={c.editor.tree.list}
+                    rootId={"root"}
+                    insertDroppableFirst={false}
+                    classes={{
+                      container: "flex flex-col",
+                      dropTarget: "drop-target",
+                      placeholder: "placeholder",
+                    }}
+                    dragPreviewRender={DragPreview}
+                    dropTargetOffset={10}
+                    placeholderRender={(node, params) => (
+                      <Placeholder node={node} params={params} />
+                    )}
+                    sort={false}
+                    onDragStart={(node) => {
+                      if (node.data?.content) {
+                        local.childs = walk(node.data?.content);
                       }
-                    }
-                  } else {
-                    const childs = (c.root as MRoot).get("childs");
-                    childs?.insert(relativeIndex || 0, [insert]);
-                  }
-                  c.render();
-                }
-              }}
-              render={(node, { depth, isOpen, onToggle }) => {
-                if (node.id === "root") return <></>;
-                return (
-                  <CETreeItem
-                    {...{
-                      id,
-                      node,
-                      depth,
-                      isOpen,
-                      onToggle,
-                      select: (item) => {
-                        c.editor.active = item;
-                        const focusText = (item: MContent) => {
-                          if (item.get("type") === "text") {
-                            setTimeout(() => {
-                              if (c.editor.activeEl?.focus) {
-                                c.editor.activeEl.focus();
-                              }
-                            }, 100);
+                    }}
+                    canDrop={(_, { dragSource, dropTargetId, dropTarget }) => {
+                      const parentSource = dragSource?.data?.content.parent
+                        .parent as MContent;
+                      if (parentSource.get("id") === "root") {
+                        return false;
+                      }
+                      if (dropTargetId === "root") {
+                        if (
+                          dragSource?.data &&
+                          dragSource.data.content.get("type") === "section"
+                        ) {
+                          return true;
+                        }
+                        return false;
+                      } else if (dragSource?.data && dropTarget?.data) {
+                        const from = dragSource.data.content.get("type");
+                        const to = dropTarget.data.content.get("type");
+
+                        if (
+                          local.childs.includes(
+                            dropTarget.data.content.get("id") || ""
+                          )
+                        ) {
+                          return false;
+                        }
+
+                        if (from === "section" || to === "text") {
+                          return false;
+                        } else if (from === "item") {
+                          if (to === "section" || to === "item") {
+                            return true;
+                          } else {
+                            return false;
                           }
-                        };
-                        focusText(c.editor.active);
+                        } else if (from === "text") {
+                          if (to === "item") {
+                            return true;
+                          }
+                        }
+
+                        return false;
+                      }
+                    }}
+                    onDrop={(
+                      _,
+                      { dropTarget, dropTargetId, dragSource, relativeIndex }
+                    ) => {
+                      if (
+                        dragSource?.data &&
+                        (dropTarget?.data || dropTargetId === "root")
+                      ) {
+                        const from = dragSource.data;
+                        const insert = from.content.clone();
+                        from.content.parent.forEach((e, idx) => {
+                          if (e === from.content) {
+                            from.content.parent.delete(idx);
+                          }
+                        });
+
+                        if (dropTargetId !== "root") {
+                          const to = dropTarget?.data;
+                          if (to) {
+                            const childs = to.content.get("childs");
+                            if (
+                              childs &&
+                              childs.length - 1 >= (relativeIndex || 0)
+                            ) {
+                              childs?.insert(relativeIndex || 0, [insert]);
+                            } else {
+                              childs?.push([insert]);
+                            }
+                          }
+                        } else {
+                          const childs = (c.root as MRoot).get("childs");
+                          childs?.insert(relativeIndex || 0, [insert]);
+                        }
                         c.render();
-                      },
+                      }
+                    }}
+                    render={(node, { depth, isOpen, onToggle }) => {
+                      if (node.id === "root") return <></>;
+                      return (
+                        <CETreeItem
+                          {...{
+                            id,
+                            node,
+                            depth,
+                            isOpen,
+                            onToggle,
+                            select: (item) => {
+                              c.editor.active = item;
+                              const focusText = (item: MContent) => {
+                                if (item.get("type") === "text") {
+                                  setTimeout(() => {
+                                    if (c.editor.activeEl?.focus) {
+                                      c.editor.activeEl.focus();
+                                    }
+                                  }, 100);
+                                }
+                              };
+                              focusText(c.editor.active);
+                              c.render();
+                            },
+                          }}
+                        />
+                      );
                     }}
                   />
-                );
-              }}
-            />
-          </div>
-        </DndProvider>
+                </div>
+              </DndProvider>
+            </>
+          )}
+        </ErrorBoundary>
       </div>
     </div>
   );
