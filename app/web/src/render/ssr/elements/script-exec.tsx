270dce8c3ad4b57284863f430181da34ba5004fe
Rizky <rizky05@gmail.com>
2023-09-04 06:14:06 +0700

    fix ssr

--- app/web/src/render/ssr/elements/script-exec.tsx
+++ app/web/src/render/ssr/elements/script-exec.tsx
@@ -56,9 +56,21 @@ const produceEvalArgs = (
   const PassProp = meta.passprop;
   const Local = meta.local;
   const PassChild = meta.passchild;
+
+  if (typeof item.nprops === "object" && !Array.isArray(item.nprops)) {
+    for (const [_, v] of Object.entries(item.nprops)) {
+      if (isValidElement(v) && v.props) {
+        const props = v.props as any;
+        if (props.item) {
+          props.item.nprops = item.nprops;
+        }
+      }
+    }
+  }
+
   const scopeProps = {
     ...window.exports,
-    ...arg.item.nprops,
+    ...item.nprops,
     params: w.params,
   };
 
