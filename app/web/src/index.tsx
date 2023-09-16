4e2109b5b63ba8fe86dcf04d92bf05096d156ac5
Rizky <rizky05@gmail.com>
2023-09-17 00:36:32 +0700

    fix

--- app/web/src/index.tsx
+++ app/web/src/index.tsx
@@ -4,12 +4,9 @@ import "./index.css";
 
 const g = (isSSR ? {} : window) as any;
 
-console.log("mantap jiwa");
-
 g.siteApiUrl = "https://api.prasi.app";
 // g.siteApiUrl = "http://localhost:12300";
 initApp("web", App);
-
 
 declare global {
   const siteApiUrl: string;
