40e1b5b09c4f076c8c1e7904e0ad38d72b87e7a1
Rizky <rizky05@gmail.com>
2023-09-29 10:52:46 +0700

    move generator top

--- app/web/src/render/editor/panel/side/props/CPMaster.tsx
+++ app/web/src/render/editor/panel/side/props/CPMaster.tsx
@@ -483,6 +483,15 @@ const SinglePopover: FC<{
           {type !== "content-element" && (
             <>
               <div className="border-t border-slate-300 pl-2 pt-1 flex justify-between items-center">
+                <div className="border-t border-slate-300 pl-2 pt-1 flex justify-between items-center">
+                  <div className="uppercase text-xs">Generator</div>
+                  <div
+                    className="m-1 px-1 bg-white cursor-pointer hover:bg-blue-500 hover:text-white hover:border-blue-500 font-mono border border-slate-300 text-[11px]"
+                    onClick={createEditScript("master-gen")}
+                  >
+                    EDIT CODE
+                  </div>
+                </div>
                 <div className="uppercase text-xs text-slate-500">Visible</div>
 
                 <div
@@ -498,15 +507,6 @@ const SinglePopover: FC<{
                 <div
                   className="m-1 px-1 bg-white cursor-pointer hover:bg-blue-500 hover:text-white hover:border-blue-500 font-mono border border-slate-300 text-[11px]"
                   onClick={createEditScript("master-value")}
-                >
-                  EDIT CODE
-                </div>
-              </div>
-              <div className="border-t border-slate-300 pl-2 pt-1 flex justify-between items-center">
-                <div className="uppercase text-xs">Generator</div>
-                <div
-                  className="m-1 px-1 bg-white cursor-pointer hover:bg-blue-500 hover:text-white hover:border-blue-500 font-mono border border-slate-300 text-[11px]"
-                  onClick={createEditScript("master-gen")}
                 >
                   EDIT CODE
                 </div>
