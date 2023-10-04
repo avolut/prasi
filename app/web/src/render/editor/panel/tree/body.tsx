aaa75f22801b4ccf9555e01e71828b31e07ee2a2
Rizky <rizky05@gmail.com>
2023-10-04 13:00:07 +0700

    fix body

--- app/web/src/render/editor/panel/tree/body.tsx
+++ app/web/src/render/editor/panel/tree/body.tsx
@@ -311,47 +311,49 @@ export const ETreeBody: FC<{ tree: NodeModel<NodeMeta>[]; meta?: any }> = ({
                       console.log(e);
                     }
                   }
-                  if (!visible)
-                    return (
-                      <div className="flex text-[8px] h-[14px] items-center">
-                        <div
-                          className={cx(
-                            css`
-                              padding-left: ${Math.round(
-                                depth < 3
-                                  ? depth * DEPTH_WIDTH
-                                  : 3 * DEPTH_WIDTH +
-                                      ((depth - 3) * DEPTH_WIDTH) / 2
-                              )}px;
-                            `
-                          )}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            onToggle();
-                          }}
-                        ></div>
-                        <div className={cx("text-purple-600", "mx-[5px]")}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={8}
-                            height={8}
-                            fill="none"
-                            viewBox="0 0 15 15"
-                          >
-                            <path
-                              fill="currentColor"
-                              fillRule="evenodd"
-                              d="M7.289.797a.5.5 0 01.422 0l6 2.8A.5.5 0 0114 4.05v6.9a.5.5 0 01-.289.453l-6 2.8a.5.5 0 01-.422 0l-6-2.8A.5.5 0 011 10.95v-6.9a.5.5 0 01.289-.453l6-2.8zM2 4.806L7 6.93v6.034l-5-2.333V4.806zm6 8.159l5-2.333V4.806L8 6.93v6.034zm-.5-6.908l4.772-2.028L7.5 1.802 2.728 4.029 7.5 6.057z"
-                              clipRule="evenodd"
-                            ></path>
-                          </svg>
-                        </div>
-                        <div>{mmeta.item.name}</div>
-                        <div className="flex-1 text-right text-slate-500 pr-1">
-                          [hidden]
-                        </div>
-                      </div>
-                    );
+                  if (!visible) {
+                    return <></>;
+                    // return (
+                    //   <div className="flex text-[8px] h-[14px] items-center">
+                    //     <div
+                    //       className={cx(
+                    //         css`
+                    //           padding-left: ${Math.round(
+                    //             depth < 3
+                    //               ? depth * DEPTH_WIDTH
+                    //               : 3 * DEPTH_WIDTH +
+                    //                   ((depth - 3) * DEPTH_WIDTH) / 2
+                    //           )}px;
+                    //         `
+                    //       )}
+                    //       onClick={(e) => {
+                    //         e.stopPropagation();
+                    //         onToggle();
+                    //       }}
+                    //     ></div>
+                    //     <div className={cx("text-purple-600", "mx-[5px]")}>
+                    //       <svg
+                    //         xmlns="http://www.w3.org/2000/svg"
+                    //         width={8}
+                    //         height={8}
+                    //         fill="none"
+                    //         viewBox="0 0 15 15"
+                    //       >
+                    //         <path
+                    //           fill="currentColor"
+                    //           fillRule="evenodd"
+                    //           d="M7.289.797a.5.5 0 01.422 0l6 2.8A.5.5 0 0114 4.05v6.9a.5.5 0 01-.289.453l-6 2.8a.5.5 0 01-.422 0l-6-2.8A.5.5 0 011 10.95v-6.9a.5.5 0 01.289-.453l6-2.8zM2 4.806L7 6.93v6.034l-5-2.333V4.806zm6 8.159l5-2.333V4.806L8 6.93v6.034zm-.5-6.908l4.772-2.028L7.5 1.802 2.728 4.029 7.5 6.057z"
+                    //           clipRule="evenodd"
+                    //         ></path>
+                    //       </svg>
+                    //     </div>
+                    //     <div>{mmeta.item.name}</div>
+                    //     <div className="flex-1 text-right text-slate-500 pr-1">
+                    //       [hidden]
+                    //     </div>
+                    //   </div>
+                    // );
+                  }
                 }
               }
 
