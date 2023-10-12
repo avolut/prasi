bb2937f1c4ec16b2ebc26d0d77f5a7be29fdd8af
Rizky <rizky05@gmail.com>
2023-10-12 16:45:54 +0700

    fix site dts

--- app/srv/api/site-dts.ts
+++ app/srv/api/site-dts.ts
@@ -1,10 +1,28 @@
 import { apiContext } from "service-srv";
 import ts from "typescript";
 import { createHash } from "crypto";
+
+const dts = {} as Record<string, { etag: string; dts: string }>;
+
 export const _ = {
   url: "/site-dts/:site_id",
   async api(site_id: string) {
     const { req, res } = apiContext(this);
+
+    let sent = false;
+    if (dts[site_id]) {
+      res.setHeader("etag", dts[site_id].etag);
+
+      if (dts[site_id].etag === req.headers["if-none-match"]) {
+        res.sendStatus(304);
+        sent = true;
+      }
+
+      if (!sent) {
+        sent = true;
+        res.send(dts[site_id].dts);
+      }
+    }
 
     const site = await db.site.findFirst({
       where: { id: site_id },
@@ -17,19 +35,24 @@ export const _ = {
         declaration: true,
       };
 
-      let dts = "";
+      let dtsrc = "";
       const host = ts.createCompilerHost(options);
-      host.writeFile = (fileName, contents) => (dts = contents);
+      host.writeFile = (fileName, contents) => (dtsrc = contents);
       host.readFile = () =>
         (site.js || "") + "\n\nexport const ______: string;";
 
       const program = ts.createProgram(["sitedts"], options, host);
       program.emit();
 
-      const etag = createHash("md5").update(dts).digest("hex");
-      res.setHeader("etag", etag);
+      const etag = createHash("md5").update(dtsrc).digest("hex");
+      if (!sent) res.setHeader("etag", etag);
 
-      return dts;
+      dts[site_id] = { etag, dts: dtsrc };
+
+      if (sent) {
+        return "";
+      }
+      return dtsrc;
     }
     res.setHeader("etag", "empty");
 
